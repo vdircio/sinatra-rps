3220882c03d5afa9d906193ea73c87cb8eaf8a22
Victor Dircio <73125400+vdircio@users.noreply.github.com>
2024-07-27 01:40:24 +0000

    added erb files

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @move = moves.sample
+  erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  @move = moves.sample
+  erb(:paper)
+end
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @move = moves.sample
+  erb(:scissors)
 end
